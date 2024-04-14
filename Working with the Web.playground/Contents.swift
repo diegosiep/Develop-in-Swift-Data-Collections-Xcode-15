03068154f39ec61194c91e81ca2bc198f904cd15
Diego Sierra <diegosiep@icloud.com>
2024-04-13 18:23:28 -0600

    Finish lesson 2.5 'Decoding JSON'

--- Working with the Web.playground/Contents.swift
+++ Working with the Web.playground/Contents.swift
@@ -1,20 +1,47 @@
 import UIKit
 
-var urlComponents = URLComponents(string: "https://api.nasa.gov/planetary/apod")
-urlComponents?.queryItems = [
-    "api_key": "DEMO_KEY",
-    "date": ""
-].map{ URLQueryItem(name: $0.key, value: $0.value) }
-
-Task {
-    let (data, response) = try await URLSession.shared.data(from: urlComponents!.url!)
-    if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 {
-        let string = String(data: data, encoding: .utf8)
-        print(string)
-    } else {
-        print("error")
+struct PhotoInfo: Codable {
+    var title: String
+    var description: String
+    var url: URL
+    var copyright: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case title
+        case description = "explanation"
+        case url
+        case copyright
     }
 }
 
+enum PhotoInfoError: Error, LocalizedError {
+    case itemNotFound
+}
+
+func fetchPhotoInfo() async throws -> PhotoInfo {
+    var urlComponents = URLComponents(string: "https://api.nasa.gov/planetary/apod")
+    urlComponents?.queryItems = [
+        "api_key": "DEMO_KEY",
+    ].map{ URLQueryItem(name: $0.key, value: $0.value) }
+    
+    let (data, response) = try await URLSession.shared.data(from: urlComponents!.url!)
+    
+    guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else { throw PhotoInfoError.itemNotFound }
+    
+    let jsonDecoder = JSONDecoder()
+    let photoInfo = try jsonDecoder.decode(PhotoInfo.self, from: data)
+    return photoInfo
+}
+
+Task {
+    do {
+        let photoInfo = try await fetchPhotoInfo()
+        print("Succesfully fethced PhotoInfo: \(photoInfo)")
+    } catch {
+        print("Fetch PhotoInfo failed with error: \(error)")
+    }
+}
 
 // 'Parse' means in programming, to convert information represented in one form into another form that's easier to work with. You take some data type and transform it into another data type.
+
+
--- iTunes Search.playground/Contents.swift
+++ iTunes Search.playground/Contents.swift
@@ -1,22 +1,100 @@
 import UIKit
 
-let url = URL(string: "https://itunes.apple.com/search")
+struct StoreItem: Codable {
+    var trackName: String
+    var artistName: String
+    var kind: String
+    var description: String
+    var artworkUrl: URL
+    
+    enum CodingKeys: String, CodingKey {
+        case trackName
+        case artistName
+        case kind
+        case description
+        case artworkUrl = "artworkUrl30"
+        
+    }
+    enum AdditionalKeys: CodingKey {
+        case longDescription
+    }
+    
+    init(from decoder: any Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        self.trackName = try values.decode(String.self, forKey: .trackName)
+        self.artistName = try values.decode(String.self, forKey: .artistName)
+        self.kind = try values.decode(String.self, forKey: .kind)
+        self.artworkUrl = try values.decode(URL.self, forKey: .artworkUrl)
+        
+        if let description = try? values.decode(String.self, forKey: CodingKeys.description) {
+            self.description = description
+        } else {
+            let additionalValues = try decoder.container(keyedBy: AdditionalKeys.self)
+            description = (try? additionalValues.decode(String.self, forKey: AdditionalKeys.longDescription)) ?? ""
+        }
+    }
+}
 
-var urlComponents = URLComponents(string: url!.absoluteString)
+struct SearchResponse: Codable {
+    let results: [StoreItem]
+}
 
-urlComponents?.queryItems = [
-    "term": "Folke Grasbeck",
-    "media": "music"
-].map {
-    URLQueryItem(name: $0.key, value: $0.value)
+enum SearchResponseError: Error, LocalizedError {
+    case couldNotCreateRequest
+}
+func fetchItems(matching query: [String: String]) async throws -> [StoreItem] {
+    let url = URL(string: "https://itunes.apple.com/search")
+    var urlComponents = URLComponents(string: url!.absoluteString)
+    
+    urlComponents?.queryItems = query.map {
+        URLQueryItem(name: $0.key, value: $0.value)
+    }
+    
+    let (data, response) = try await URLSession.shared.data(from: (urlComponents!.url!))
+    guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+        throw SearchResponseError.couldNotCreateRequest
+    }
+    
+    let jsonDecoder = JSONDecoder()
+    let searchResponse = try jsonDecoder.decode(SearchResponse.self, from: data)
+    
+    return searchResponse.results
+    
 }
 
 Task {
-    let (data, response) = try await URLSession.shared.data(from: urlComponents!.url!)
-    
-    if let response = response as? HTTPURLResponse, response.statusCode == 200 {
-        let data = String(data: data, encoding: .utf8)
-        print(data!)
+    do {
+        let searchItems = try await fetchItems(matching: [
+            "term": "Diego Sierra Sibelius",
+            "media": "music",
+            "limit": "10"
+        ])
+        
+        searchItems.forEach { searchItem in
+            print("""
+                  Artist: \(searchItem.artistName)
+                  Song: \(searchItem.trackName)
+                  Artwork URL: \(searchItem.artworkUrl)
+""")
+        }
+    } catch {
+        print("error")
     }
 }
+
+extension Data {
+    func prettyPrintedJSONString() {
+        guard
+            let jsonObject = try?
+                JSONSerialization.jsonObject(with: self, options: []),
+            let jsonData = try?
+                JSONSerialization.data(withJSONObject: jsonObject, options: [.prettyPrinted]),
+            let prettyJSONString = String(data: jsonData, encoding: .utf8) else {
+            print("Failed to read JSON object")
+            return
+        }
+        print(prettyJSONString)
+        
+    }
     
+}
